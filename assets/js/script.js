9473d642e7e0d75e64672053a4e78e80fe449baa
Jake <jacob.watson00@yahoo.com>
2024-09-28 01:15:25 -0600

    finished the button design, except for media queries. Add javascript functionality to navigate to various pages.

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+document.getElementById('latest-episode').addEventListener('click', function() {
+    window.location.href = 'latest-episode.html';
+});
+
+document.getElementById('all-episodes').addEventListener('click', function() {
+    window.location.href = 'all-episodes.html';
+});
+
+document.getElementById('store').addEventListener('click', function() {
+    window.location.href = 'store.html';
+});
+
+document.getElementById('our-story').addEventListener('click', function() {
+    window.location.href = 'our-story.html';
+});
+
